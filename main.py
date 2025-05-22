af8b24167306c8f82f251114678e4e788838ef7f
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-22 08:50:33 +0200

    add window game

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pygame
+from pygame.examples.setmodescale import screen
+
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # initialisation de pygame
+    pygame.init()
+    # definition de la taille de la fenetre
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # boucle infinie du jeu
+    while True:
+        # permet de fermer le programme en quittant la fenetre
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # on set la couleur a noir
+        screen.fill("black")
+        # on refresh le screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
         main()
